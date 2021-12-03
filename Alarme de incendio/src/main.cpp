5679919749aae61de56214bea6baf81cd6b21430
Thiago dos santos <thiago dos santos>
2021-12-03 19:23:33 -0300

    atualização

--- Alarme de incendio/src/main.cpp
+++ Alarme de incendio/src/main.cpp
@@ -1,29 +1,26 @@
 #include <Arduino.h>
-int Sensor = 4;
-int Buzzer = 16;
-int pinoLed = 15;
-int Var = 0;
+int Sensor = 4; // declaração da pinagem do sensor de chamafogo//
+int Buzzer = 16;// declaração da pinagem do buzzer//
+int pinoLed = 15;//declaração da pinagem do led//
+int Var;// inicialmente a entrada do sensor é um//
  
 void setup() 
 { 
- pinMode(Buzzer, OUTPUT);
- pinMode(Sensor, INPUT);
- pinMode(pinoLed, OUTPUT);
- Serial.begin(9600);
+ pinMode(Buzzer, OUTPUT);// declaro que o pino do buzzer é de saída//
+ pinMode(Sensor, INPUT);//declaro que o pino do sensor é de entrada//
+ pinMode(pinoLed, OUTPUT); // declaro que o pino do led é de saída//
+ Serial.begin(9600);// inicia a porta serial, configura a taxa de dados para 9600 bps//
 } 
  
 void loop() 
 { 
- Var=digitalRead(Sensor);
- Serial.print(Var);
- if(Var<1) 
+ Var=digitalRead(Sensor);// leitura do pino digital do sensor//
+ if(Var<1) // se a leitura do sensor for menor que 1 ele roda a sequência//
  {
- digitalWrite(Buzzer, HIGH);
- digitalWrite(pinoLed, HIGH);
- delay(200);
- digitalWrite(Buzzer, LOW);
- digitalWrite(pinoLed, LOW);
- delay(20);
+ digitalWrite(Buzzer, HIGH);// liga o pino digital do buzzer em alto//
+ digitalWrite(pinoLed, HIGH);// liga o led//
+ delay(1000);// espera por um 1 segundos
+ digitalWrite(Buzzer, LOW);// desliga o buzzer//
+ digitalWrite(pinoLed, LOW);//desliga o led//
  } 
  
-}
